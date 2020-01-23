f3cae74a4808435140fd36c6897caa971a668679
Dave Chevell <dchevell@atlassian.com>
2020-01-23 20:29:44 +1100

    Remove Alpine images and switch Docker Hub target to atlassian/crowd

--- entrypoint.py
+++ entrypoint.py
@@ -9,11 +9,6 @@ CROWD_INSTALL_DIR = env['crowd_install_dir']
 CROWD_HOME = env['crowd_home']
 
 gen_cfg('server.xml.j2', f'{CROWD_INSTALL_DIR}/apache-tomcat/conf/server.xml')
-# gen_cfg('container_id.j2', '/etc/container_id')
-# gen_cfg('dbconfig.xml.j2', f'{CROWD_HOME}/dbconfig.xml',
-#         user=RUN_USER, group=RUN_GROUP, overwrite=False)
-# gen_cfg('cluster.properties.j2', f'{CROWD_HOME}/cluster.properties',
-#         user=RUN_USER, group=RUN_GROUP, overwrite=False)
 
 start_app(f'{CROWD_INSTALL_DIR}/start_crowd.sh -fg', CROWD_HOME, name='Crowd')
 
